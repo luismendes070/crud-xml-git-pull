824ee2f6ff79501648ef9f885f21c7b177236caf
Luis Mendes <machadoluismendes@gmail.com>
2023-02-05 09:25:18 -0300

    Increment
    courses XML mock frontend angular xml2js testing

--- crud-angular-spring/crud-angular/tests/ChaiLibraryAssertionsTest.ts
+++ crud-angular-spring/crud-angular/tests/ChaiLibraryAssertionsTest.ts
@@ -2,8 +2,9 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 const url: string = "https://www.localhost:4200/api/cursos";
-const path: string = "https://www.localhost:4200/api/cursos";
-const data: string = "https://www.localhost:4200/api/cursos";
+const path: string = require("./courses.xml");
+// const parse: string = path.parse();
+const data: string = '<courses><course category="backend"><id>123456789</id><name>Course Name</name></course><course category="frontend"><id>223456789</id><name>Course Name 2</name></course></courses>';
 
 chai.use(chaiHttp);
 
--- crud-angular-spring/crud-angular/tests/XML2JsTest.ts
+++ crud-angular-spring/crud-angular/tests/XML2JsTest.ts
@@ -1,5 +1,11 @@
 const xml2js = require('xml2js');
 
+const url: string = 'https://www.localhost:4200/api/cursos';
+const path: string = require('./courses.xml');
+// const parse: string = path.parse();
+const data: string =
+  '<courses><course category="backend"><id>123456789</id><name>Course Name</name></course><course category="frontend"><id>223456789</id><name>Course Name 2</name></course></courses>';
+
 // parse the XML response
 xml2js.parseString(res.text, (err, result) => {
   expect(err).to.be.null;
