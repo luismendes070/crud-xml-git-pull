7a27d5956e3cd8c604fd6050bc25d1d54ceba240
Luis Mendes <machadoluismendes@gmail.com>
2023-02-09 10:44:47 -0300

    Merge branch 'master' of https://github.com/luismendes070/curso-angular

--- crud-angular-spring/crud-angular/src/app/app.module.ts
+++ crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
