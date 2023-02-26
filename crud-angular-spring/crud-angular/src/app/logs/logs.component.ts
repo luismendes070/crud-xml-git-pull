db271d9a7956f35a31a694b2ccd39ec2ca39b8ee
Luis Mendes <machadoluismendes@gmail.com>
2023-02-26 09:47:51 -0300

    scss typo fix css

--- crud-angular-spring/crud-angular/src/app/logs/logs.component.ts
+++ crud-angular-spring/crud-angular/src/app/logs/logs.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eb248a7e9cac6f35e30d5c3fc2b2c4d858a8a89be20d2bcabb04bb622de107d
-size 501
+oid sha256:323ae53fa87e231f027fa861a3d1c0f48ffe148d52f7ea4fa68c3b90d536261d
+size 502
