f434e6332d8f647ccb7d89fffce91c2da269335f
Luis Mendes <machadoluismendes@gmail.com>
2023-02-26 11:53:31 -0300

    install postgres SQL Ubuntu 22 shell script

---
+++ crud-angular-spring/crud-angular/tests/install_postgresql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install PostgreSQL
+sudo apt install postgresql postgresql-contrib
+
+# Start the PostgreSQL service
+sudo systemctl start postgresql
+
+# Enable PostgreSQL to start at boot time
+sudo systemctl enable postgresql
+
+# Check the status of the PostgreSQL service
+sudo systemctl status postgresql
+
+# executable Ubuntu 22 permission
+# chmod +x install_postgresql.sh
--- crud-angular-spring/crud-spring/README.md
+++ crud-angular-spring/crud-spring/README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2c0ac81c92fb7369985ac7d48ceca95f37f54197db17eed24c9029ce026d9a7
-size 17
+oid sha256:66105483607fe567f2cf07b7ff7dac68c35c433ff209d20de70ef662a996efbf
+size 91
