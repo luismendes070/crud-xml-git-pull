e156fe2efa4a45c2e5a5680507857975a47c1140
Luis Mendes <machadoluismendes@gmail.com>
2023-02-26 08:10:28 -0300

    TypeScript any undefined data types

--- crud-angular-spring/crud-angular/tests/JsMockitoInsomniaRestClientTest.ts
+++ crud-angular-spring/crud-angular/tests/JsMockitoInsomniaRestClientTest.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:446cbeaf0cfd29965c721f76d0c557fa4ec254f40768d543f97052adabaaa56c
-size 1114
+oid sha256:9ac2ae370a4b440177d5fcfd8b97edaa4598cd731e95f75e36d4eb13cfced4da
+size 1122
--- crud-angular-spring/crud-angular/tests/XML2JsTest.ts
+++ crud-angular-spring/crud-angular/tests/XML2JsTest.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bce53d85666235214a2016fd4861781fac340b5de8eaa18d6a6d299e151bb804
-size 587
+oid sha256:dd05511183e79df92ba4cc7006bcad9217503bc6270d63fba5ae207986867644
+size 599
