2b7fe6bb29d8de66f90a8ae69a28fd7c2ea0b805
luismendes070 <machadoluismendes@gmail.com>
2023-03-03 15:01:42 -0300

    shell scripts config

--- crud-angular-spring/crud-angular/tests/install_git_lfs.sh
+++ crud-angular-spring/crud-angular/tests/install_git_lfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
 git lfs track "*.java"
 git lfs track "*.js"
 git lfs track "*.json"
