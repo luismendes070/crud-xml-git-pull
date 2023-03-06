ed62d387329357e5dff74ced99ff6ef6af6d4c68
luismendes070 <machadoluismendes@gmail.com>
2023-03-06 12:49:52 -0300

    Angular soap web services any data type fix fix

--- crud-angular-spring/crud-angular/src/app/soap/soap-service.service.ts
+++ crud-angular-spring/crud-angular/src/app/soap/soap-service.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ffb266a15880d0d4c0d83f9b8e25e6d662d74cd9904448e6708fa4eafb51cbb
-size 1142
+oid sha256:93693ddc8715797314d34606b8a3438eab5a3456b3f1e1ef5b917b75e9d83922
+size 1125
