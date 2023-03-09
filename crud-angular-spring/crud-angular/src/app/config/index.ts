8251004a21d908f39f6d28b950845645e3448408
luismendes070 <machadoluismendes@gmail.com>
2023-03-09 13:07:49 -0300

    npm libraries dotenv config isntalled

---
+++ crud-angular-spring/crud-angular/src/app/config/index.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4df22e5b1a96f9bfb632be3ad0ac42c75dff9db60dd4d9413813e0c40fd80a7a
+size 243
--- crud-angular-spring/crud-spring/src/main/java/com/loiane/config/SecurityConfiguration.java
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/config/SecurityConfiguration.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac340fe2405ea06f131754c6269babc2d927260034ce48293512ac155949d529
-size 931
+oid sha256:8d4a83aedadb83e1f8ad29d57b7c35e453229417bf3cec7aee3d1b841387db07
+size 999
