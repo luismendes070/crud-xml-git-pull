4b29596756117fdbd14dea147f86f96e87626714
luismendes070 <machadoluismendes@gmail.com>
2023-03-09 14:50:59 -0300

    ${ENDPOINT} environment variable

--- crud-angular-spring/crud-angular/src/app/soap/soap-service.service.ts
+++ crud-angular-spring/crud-angular/src/app/soap/soap-service.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93693ddc8715797314d34606b8a3438eab5a3456b3f1e1ef5b917b75e9d83922
-size 1125
+oid sha256:e3e190cd42e4adba7dd6a47ed0d18847a8a54056f35fe86ad733d0190f7965e2
+size 1131
