1661cedbb77c5d5c54dd7f357c41fccc1913eb98
Luis Mendes <machadoluismendes@gmail.com>
2023-10-11 08:18:02 -0300

    Increment
    @Test
    public void testPatchXml() throws Exception {
        mockMvc.perform(patch("/api/example")
                        .contentType(MediaType.APPLICATION_XML)
                        .content("<request>example request</request>"))
                .andExpect(status().isOk());
    }

--- crud-angular-spring/crud-spring/src/test/java/com/loiane/CrudSpringApplicationTests.java
+++ crud-angular-spring/crud-spring/src/test/java/com/loiane/CrudSpringApplicationTests.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:400201950f52fd127523a40f83d6a6f73d0ab32554a021939eacb5e0be1c5dc5
-size 1626
+oid sha256:5c1bbb33b0b7df55338424e22c8b4ffde019593989e245d2bed911d505be18b1
+size 1805
